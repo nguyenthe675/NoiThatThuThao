ef78917d3a1ea59d7ad0e103ce6de9302c542035
nguyenthe675 <nguyenthe675@gmail.com>
2018-06-07 22:53:15 +0700

    Init project

---
+++ SHY.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace SHY.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private ShyDbContext dbContext;
+
+        public ShyDbContext Init()
+        {
+            return dbContext ?? (dbContext = new ShyDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
---
+++ SHY.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHY.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private ShyDbContext dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public ShyDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
